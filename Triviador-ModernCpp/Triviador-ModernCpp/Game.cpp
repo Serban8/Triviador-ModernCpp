bc4759a8673ce8f7a9a183888d1f856b914b57ff
AlexAntohi <alexandru.antohi@student.unitbv.ro>
2022-11-20 19:53:09 +0200

    [#9] Redesigned Game class. Now he have separate vectors for number questions and multiple choice questions and started to implement map logic.

--- Triviador-ModernCpp/Triviador-ModernCpp/Game.cpp
+++ Triviador-ModernCpp/Triviador-ModernCpp/Game.cpp
@@ -1,26 +1,43 @@
 #include "Game.h"
+#include "QuestionGenerator.h"
 
-Game::Game(std::vector<Player> players, std::vector<AbstractQuestion*> questions) :
+Game::Game(std::vector<Player> players) :
+	m_activePlayers(players)
 	
-	m_activePlayers(players),
-	m_questions(questions)
 {
+	//getting questions directly from online database;
+	//in future we need to change this to get them from our local database;
+	QuestionGenerator qg;
+	m_multipleChoiceQuestions = qg.GenerateMultipleChoiceQuestions(50);
+	//m_numberQuestions = qg.GenerateNumberQuestions(50);
+	switch (players.size()) {
+	case 2:
+		m_map.resize(3);
+		for (auto& line : m_map) {
+			line.resize(3);
+		}
+		break;
+	case 3:
+		m_map.resize(5);
+		for (auto& line : m_map) {
+			line.resize(3);
+		}
+		break;
+	case 4:
+		m_map.resize(6);
+		for (auto& line : m_map) {
+			line.resize(4);
+		}
+		break;
+	default:
+		throw std::runtime_error("Number of players has to be between 2 and 4.");
+	}
 }
 
 
 std::vector<Player> Game::GetPlayers()
 {
 	return m_activePlayers;
-}
-
-std::vector<AbstractQuestion*> Game::GetQuestions()
-{
-	return m_questions;
-}
-
-void Game::SetQuestions(std::vector<AbstractQuestion*> questions)
-{
-	m_questions = questions;
 }
 
 void Game::AddInactivePlayer(Player player)
--- Triviador-ModernCpp/Triviador-ModernCpp/Game.h
+++ Triviador-ModernCpp/Triviador-ModernCpp/Game.h
@@ -1,36 +1,30 @@
 #pragma once
 #include <iostream>
 #include <vector>
-#include "AbstractQuestion.h"
+#include "MultipleChoiceQuestion.h"
+#include "NumberQuestion.h"
 #include "Player.h"
+#include "Region.h"
 
 class Game
 {
 public:
 
-	Game(std::vector<Player> players, std::vector<AbstractQuestion*> questions);
+	Game(std::vector<Player> players);
 
 	//getters
 	std::vector<Player> GetPlayers();
-	std::vector<AbstractQuestion*> GetQuestions();
-
-	//setters
-	void SetQuestions(std::vector<AbstractQuestion*> questions);
 
 	//methods
 	void AddInactivePlayer(Player player);
 
 private:
 
-	int m_id;
-
-	std::string m_dateAndTime;
 	std::vector<Player> m_activePlayers;
 	std::vector<Player> m_inactivePlayers;
-	std::vector<AbstractQuestion*> m_questions;
-
-	//Map m_map;
-	//
+	//std::vector<NumberQuestion> m_numberQuestions;
+	std::vector<MultipleChoiceQuestion> m_multipleChoiceQuestions;
+	std::vector<std::vector<Region>> m_map;
 
 };
 
