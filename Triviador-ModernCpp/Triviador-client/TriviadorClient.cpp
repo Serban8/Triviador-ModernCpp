4339122822a298ca175f97fa6ed034a27e244847
denis-adelin <denis.bardas@student.unitbv.ro>
2022-12-19 00:17:19 +0200

    [#55] Added changes to checkIfGameCanStart function to display the list of players and their votes

--- Triviador-ModernCpp/Triviador-client/TriviadorClient.cpp
+++ Triviador-ModernCpp/Triviador-client/TriviadorClient.cpp
@@ -35,18 +35,28 @@ statusCode TriviadorClient::CreateNewPlayer(std::string& username, std::string& 
 	return response.status_code;
 }
 
-bool TriviadorClient::checkIfGameCanStart()
+void TriviadorClient::checkIfGameCanStart()
 {
-	cpr::Response response = cpr::Get(cpr::Url{ "http://localhost:18080/checkwaitingroom" });
-	auto players = crow::json::load(response.text);
-	while (players.size() < 4)
-	{
-		std::chrono::system_clock::time_point timePt = std::chrono::system_clock::now() + std::chrono::seconds(5);
-		std::this_thread::sleep_until(timePt);
-		cpr::Response response2 = cpr::Get(cpr::Url{ "http://localhost:18080/checkwaitingroom" });
-		auto players = crow::json::load(response2.text);
-		if (players.size() == 4)
-			break;
-	}
-	return true;
+    crow::json::rvalue resBody; //the list of all players and their votes and the flag for starting the game
+    do
+    {
+        cpr::Response response = cpr::Get(cpr::Url{ "http://localhost:18080/checkwaitingroom" });
+        resBody = crow::json::load(response.text);
+        std::chrono::system_clock::time_point timePt = std::chrono::system_clock::now() + std::chrono::seconds(5);
+        std::this_thread::sleep_until(timePt);
+
+        //for each player display their option
+        for (int i = 0; i < resBody.size() - 1; i++)
+        {
+            if (resBody[i]["votedToStart"] == "true")
+            {
+                std::cout << resBody[i]["username"] << " voted to start" << std::endl;
+            }
+            else
+            {
+                std::cout << resBody[i]["username"] << " did not vote" << std::endl;
+            }
+        }
+    //if the flag is set to true then the game can start
+    } while (resBody[resBody.size() - 1]["startGame"] != "true");
 }
