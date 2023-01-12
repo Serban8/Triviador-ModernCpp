31ca17c31cd57aca14a2cdc414ba6d907124de90
denis-adelin <denis.bardas@student.unitbv.ro>
2023-01-12 23:53:55 +0200

    Added vector to store the groupboxes for each region

--- Triviador-ModernCpp/Triviador-client/MapWindow.cpp
+++ Triviador-ModernCpp/Triviador-client/MapWindow.cpp
@@ -16,15 +16,79 @@ void MapWindow::SetUI(int numberOfPlayers)
 	if (numberOfPlayers == 2)
 	{
 		twoPlayersMap_ui.setupUi(this);
+		setTwoPlayersRegions();
 	}
 	else
 		if (numberOfPlayers == 3)
 		{
 			threePlayersMap_ui.setupUi(this);
+			setTwoPlayersRegions();
 		}
 		else
 			if (numberOfPlayers == 4)
 			{
 				fourPlayersMap_ui.setupUi(this);
+				setFourPlayersRegions();
+				qDebug()<<Regions[2]->children()[0]->children()[0]->children()[0];
 			}
+}
+
+void MapWindow::setTwoPlayersRegions()
+{
+	Regions.push_back(twoPlayersMap_ui.groupBox_Territory1);
+	Regions.push_back(twoPlayersMap_ui.groupBox_Territory2);
+	Regions.push_back(twoPlayersMap_ui.groupBox_Territory3);
+	Regions.push_back(twoPlayersMap_ui.groupBox_Territory4);
+	Regions.push_back(twoPlayersMap_ui.groupBox_Territory5);
+	Regions.push_back(twoPlayersMap_ui.groupBox_Territory6);
+	Regions.push_back(twoPlayersMap_ui.groupBox_Territory7);
+	Regions.push_back(twoPlayersMap_ui.groupBox_Territory8);
+	Regions.push_back(twoPlayersMap_ui.groupBox_Territory9);
+}
+
+void MapWindow::setThreePlayersRegions()
+{
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory1);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory2);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory3);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory4);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory5);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory6);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory7);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory8);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory9);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory10);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory11);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory12);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory13);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory14);
+	Regions.push_back(threePlayersMap_ui.groupBox_Territory15);
+}
+
+void MapWindow::setFourPlayersRegions()
+{
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory1);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory2);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory3);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory4);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory5);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory6);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory7);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory8);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory9);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory10);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory11);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory12);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory13);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory14);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory15);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory16);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory17);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory18);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory19);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory20);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory21);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory22);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory23);
+	Regions.push_back(fourPlayersMap_ui.groupBox_Territory24);
 }
